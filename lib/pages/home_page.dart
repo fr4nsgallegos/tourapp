735cd8b098a71d420d29320ec0be03c7d786a96f
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-02 10:11:34 -0500

    init page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:tourapp/pages/widgets/welcome_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: WelcomeWidget(
-            title: "Viaja con nosotros",
-            description:
-                "Phasellus sed augue vitae ex facilisis volutpat. Nullam eleifend augue lorem, in molestie leo consectetur sed. Praesent nec enim at sem interdum faucibus nec sit amet ex. Sed ullamcorper fringilla faucibus. Proin velit elit, lacinia at aliquet et, convallis sed massa. Aliquam e",
-            color: Colors.cyan,
-            pathAsset: "assets/icons/avion.png"),
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Column(),
       ),
     );
   }
---
+++ lib/pages/init_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tourapp/pages/widgets/welcome_widget.dart';
+
+class InitPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: WelcomeWidget(
+            title: "Viaja con nosotros",
+            description:
+                "Phasellus sed augue vitae ex facilisis volutpat. Nullam eleifend augue lorem, in molestie leo consectetur sed. Praesent nec enim at sem interdum faucibus nec sit amet ex. Sed ullamcorper fringilla faucibus. Proin velit elit, lacinia at aliquet et, convallis sed massa. Aliquam e",
+            color: Colors.cyan,
+            pathAsset: "assets/icons/avion.png"),
+      ),
+    );
+  }
+}
