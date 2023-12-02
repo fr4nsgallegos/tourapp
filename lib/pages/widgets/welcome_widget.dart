9c276ce8f8b6355dff92ac9d86562aa0bf0a9c0c
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-02 11:26:51 -0500

    finish get widget

--- lib/pages/widgets/welcome_widget.dart
+++ lib/pages/widgets/welcome_widget.dart
@@ -14,40 +14,43 @@ class WelcomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height - 200,
-            // height: 500,
-            width: MediaQuery.of(context).size.width - 50,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(pathAsset, height: 150),
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-                ),
-                Divider(
-                  height: 50,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Text(description),
-                ),
-              ],
-            ),
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height - 200,
+              // height: 500,
+              width: MediaQuery.of(context).size.width - 50,
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(pathAsset, height: 150),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    title,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  ),
+                  Divider(
+                    height: 50,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Text(description),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
