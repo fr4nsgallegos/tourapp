65435889adf9c33558a77eedea2d616c0ee6e06b
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-02 09:27:41 -0500

    init conf

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tourapp/pages/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
