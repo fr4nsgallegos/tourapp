82d1b0b216f66fed8ed8a2bc268cf81077a37ddf
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-03 09:31:32 -0500

    init interface

--- lib/pages/home_page_2.dart
+++ lib/pages/home_page_2.dart
@@ -7,9 +7,50 @@ class HomePage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Homepage"),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.rocket,
+              color: Colors.orange,
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              "Discount Tour",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
       ),
-      body: Column(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+              "Country",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
